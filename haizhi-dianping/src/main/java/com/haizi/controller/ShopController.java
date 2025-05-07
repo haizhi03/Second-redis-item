d20e141598ff1beb81b1d67083be765f9a2c116a
haizhi03 <167165049+haizhi03@users.noreply.github.com>
2025-05-07 21:53:49 +0800

    根据id查询商铺缓存

--- haizhi-dianping/src/main/java/com/haizi/controller/ShopController.java
+++ haizhi-dianping/src/main/java/com/haizi/controller/ShopController.java
@@ -33,7 +33,7 @@ public class ShopController {
      */
     @GetMapping("/{id}")
     public Result queryShopById(@PathVariable("id") Long id) {
-        return Result.ok(shopService.getById(id));
+        return shopService.queryById(id);
     }
 
     /**
@@ -57,10 +57,8 @@ public class ShopController {
     @PutMapping
     public Result updateShop(@RequestBody Shop shop) {
         // 写入数据库
-        shopService.updateById(shop);
-        return Result.ok();
+        return shopService.update(shop);
     }
-
     /**
      * 根据商铺类型分页查询商铺信息
      * @param typeId 商铺类型
--- haizhi-dianping/src/main/java/com/haizi/service/IShopService.java
+++ haizhi-dianping/src/main/java/com/haizi/service/IShopService.java
@@ -1,5 +1,6 @@
 package com.haizi.service;
 
+import com.haizi.dto.Result;
 import com.haizi.entity.Shop;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopService extends IService<Shop> {
 
+
+
+    Result queryById(Long id);
+
+    Result update(Shop shop);
 }
